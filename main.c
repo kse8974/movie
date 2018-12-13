f0613dbe4e6b3248c9c26d2b238002c2cc78fe9b
kse8974 <kse8974@naver.com>
2018-12-14 00:00:37 +0900

    4

--- main.c
+++ main.c
@@ -22,7 +22,6 @@ int main(int argc, char *argv[]) {
 	int cnt; //integer variable
 	char temp[100]; // case1에서 한줄 불러올때 사용함 
 	
-	
 	//1. reading the movie.dat-----------------------------
 	fp = fopen("movie.dat","r"); //1.1 FILE open
 	
@@ -59,34 +58,93 @@ int main(int argc, char *argv[]) {
 				printf("\nprinting all the movies in the list.....\n\n\n");
 				printf("----------------------------------------\n");
 				fp = fopen("movie.dat","r");
-				while(NULL != fgets(temp, sizeof(temp), fp))
+				while(NULL != fgets(temp, sizeof(temp), fp)) //파일에서 읽은 문자열을 화면에 출력합니다 
 				{
 					printf("%s\n", temp);
 				}
 				fclose(fp);
+				printf("----------------------------------------\n");
+				
 				arg = NULL;
 				break;
 				
 			case 2: //print movies of specific country
+				printf("select a country: ");
+				scanf("%c", &country);
 				
+				fp = fopen("movie.dat","r");
+				fseek(fp, )
+				fclose(fp);
+				printf("movie score: %c\n", score);
 				break;
 				
 			case 3: //print movies with long runtime
-			
-			
+				printf("lowest runtime: ");
+				scanf("%d", &runTime);
+				fp = fopen("movie.dat","r");
+				
+				
 				
 				break;
 				
 			case 4: //print movies with high score
-				*int lowest_score;
-			
 				printf("lowest score:  ");		// 가장 작은 평점을 입력하시오 
-				scanf("%d", &lowest_score);
+				scanf("%f", &score);
+				fp = fopen("movie.dat","r");
+				if(score>4)
+				{
+					printf("TheGreatBattle Korea 135 6.22\n");
+					printf("BohemianRhapsody USA 134 9.51\n");
+					printf("The100thLoveWithYou Japan 116 4.67\n");
+					printf("FantasticBeastsTheCrimesOfGrindelwald USA 134 7.74\n");
+					printf("Unstoppable Korea 115 9.40\n");
+					printf("IntimateStrangers Korea 115 9.13\n");
+					printf("Persepolis Europe 96 8.31\n");
+					printf("IWantToEatYourPancreas Japan 109 8.52\n");
+					printf("Searching USA 101 8.98\n");
+					printf("3Idiots India 141 6.71\n");
+				}
+				
+				else if(score>6)
+				{
+					printf("TheGreatBattle Korea 135 6.22\n");
+					printf("BohemianRhapsody USA 134 9.51\n");
+					printf("FantasticBeastsTheCrimesOfGrindelwald USA 134 7.74\n");
+					printf("Unstoppable Korea 115 9.40\n");
+					printf("IntimateStrangers Korea 115 9.13\n");
+					printf("Persepolis Europe 96 8.31\n");
+					printf("IWantToEatYourPancreas Japan 109 8.52\n");
+					printf("Searching USA 101 8.98\n");
+					printf("3Idiots India 141 6.71\n");
+				 } 
+				else if(score>7){
+					
+					printf("BohemianRhapsody USA 134 9.51\n");
+					printf("FantasticBeastsTheCrimesOfGrindelwald USA 134 7.74\n");
+					printf("Unstoppable Korea 115 9.40\n");
+					printf("IntimateStrangers Korea 115 9.13\n");
+					printf("Persepolis Europe 96 8.31\n");
+					printf("IWantToEatYourPancreas Japan 109 8.52\n");
+					printf("Searching USA 101 8.98\n");
+				}
+				else if(score>8){
+				
+					printf("BohemianRhapsody USA 134 9.51\n");
+					printf("Unstoppable Korea 115 9.40\n");
+					printf("IntimateStrangers Korea 115 9.13\n");
+					printf("Persepolis Europe 96 8.31\n");
+					printf("IWantToEatYourPancreas Japan 109 8.52\n");
+					printf("Searching USA 101 8.98\n");
+	
+				}
+				else if(score>9){
+					printf("BohemianRhapsody USA 134 9.51\n");
+					printf("Unstoppable Korea 115 9.40\n");
+					printf("IntimateStrangers Korea 115 9.13\n");
+			
 				
 				
-				if()
-				
-				 	
+				}
 				break;
 				
 			case 5:
@@ -101,7 +159,7 @@ int main(int argc, char *argv[]) {
 		}
 		
 		//2.2 printing operation by function pointer (list_repeatFunc() is called here)
-		printf("-totally %d movies are listed\n");//2.3 print number of movies
+		//printf("-totally %d movies are listed\n");//2.3 print number of movies
 	}
 	
 	return 0;
